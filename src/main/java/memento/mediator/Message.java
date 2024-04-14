f664ccb14e5e515f427662f403e589679d4f9a0b
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-13 17:19:56 -0700

    Add getters to Message

--- src/main/java/memento/mediator/Message.java
+++ src/main/java/memento/mediator/Message.java
@@ -4,14 +4,29 @@ import java.sql.Timestamp;
 
 public class Message
 {
-    private String content;
-    private User sender;
-    private Timestamp timestamp;
+    private final String content;
+    private final User sender;
+    private final Timestamp timestamp;
 
     public Message(String content, User sender)
     {
         this.content = content;
         this.sender = sender;
         timestamp = new Timestamp(System.currentTimeMillis());
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public User getSender()
+    {
+        return sender;
+    }
+
+    public Timestamp getTimestamp()
+    {
+        return timestamp;
     }
 }
