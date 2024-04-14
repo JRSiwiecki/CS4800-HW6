4b028a6ed00da661d71bca5d190a701e47ae0260
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-13 17:20:06 -0700

    Add getters to user

--- src/main/java/memento/mediator/User.java
+++ src/main/java/memento/mediator/User.java
@@ -1,26 +1,40 @@
 package memento.mediator;
 
+import java.util.ArrayList;
+
 public class User
 {
-    private String username;
+    private final String username;
+    private final ArrayList<Message> messageBox = new ArrayList<>();
 
     public User(String username)
     {
         this.username = username;
     }
 
-    public void sendMessage(ChatServer server, String receiver)
+    public void sendMessage(ChatServer server, String receiver, String messageContent)
     {
-        server.sendMessage(this, receiver);
+        Message message = new Message(messageContent, this);
+        server.sendMessage(this, receiver, message);
     }
 
-    public Message receiveMessage(ChatServer server)
+    public void receiveMessage(Message message)
     {
-        server.receiveMessage(this);
+        messageBox.add(message);
     }
 
     public void undoLastMessageSent(ChatServer server)
     {
         server.undoLastMessageSent(this);
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public ArrayList<Message> getMessageBox()
+    {
+        return messageBox;
     }
 }
