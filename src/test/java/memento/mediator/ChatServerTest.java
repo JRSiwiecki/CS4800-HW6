f1b95298394e847d1d9e415743eacdf4741bc791
JRSiwiecki <JRSiwiecki@gmail.com>
2024-04-15 22:14:55 -0700

    Add more tests to ChatServerTest

--- src/test/java/memento/mediator/ChatServerTest.java
+++ src/test/java/memento/mediator/ChatServerTest.java
@@ -44,10 +44,21 @@ class ChatServerTest
     }
 
     @org.junit.jupiter.api.Test
-    void sendMessage()
+    void sendMessageToOne()
     {
         user1.sendMessage(chatServer, new User[]{user2}, "message");
+
         assertEquals("message", user2.getMessageBox().get(0).getContent());
+    }
+
+    @org.junit.jupiter.api.Test
+    void sendMessageToMany()
+    {
+        User user3 = new User("C");
+        user1.sendMessage(chatServer, new User[]{user2, user3}, "message");
+
+        assertEquals("message", user2.getMessageBox().get(0).getContent());
+        assertEquals("message", user3.getMessageBox().get(0).getContent());
     }
 
     @org.junit.jupiter.api.Test
@@ -55,13 +66,26 @@ class ChatServerTest
     {
         user1.sendMessage(chatServer, new User[]{user2}, "undo");
         user1.undoLastMessageSent(chatServer);
+
         assertEquals(0, user2.getMessageBox().size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void undoMessageToMany()
+    {
+        User user3 = new User("C");
+        user1.sendMessage(chatServer, new User[]{user2}, "undo");
+        user1.undoLastMessageSent(chatServer);
+
+        assertEquals(0, user2.getMessageBox().size());
+        assertEquals(0, user3.getMessageBox().size());
     }
 
     @org.junit.jupiter.api.Test
     void blockUser()
     {
         chatServer.blockUser(user1, user2);
+
         assertTrue(chatServer.getBlockMap().get(user1).contains(user2));
     }
 
@@ -70,6 +94,7 @@ class ChatServerTest
     {
         chatServer.blockUser(user1, user2);
         chatServer.unblockUser(user1, user2);
+
         assertFalse(chatServer.getBlockMap().get(user1).contains(user2));
     }
 }
